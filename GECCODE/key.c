1bd821bcb0d9340456cbae8b5f8990dd336747e9
HKishere <939132879@qq.com>
2019-12-31 14:58:34 +0800

    delete some file that useless

--- GECCODE/key.c
+++ GECCODE/key.c
@@ -7,7 +7,6 @@ void init_key(void)
 	
 	
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE , ENABLE);
-	
 	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_3;
 	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN;//输入模式
 	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;//速度50MHz
